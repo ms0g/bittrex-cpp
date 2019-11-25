b1f03ccbc07af4d394a45351283057ef2a4fc74a
gurpinars <sami.gurpinar@gmail.com>
2019-11-25 11:14:33 +0300

    multicore compiling

--- build/builder.sh
+++ build/builder.sh
@@ -1,4 +1,14 @@
+CMAKE_PARAMS="-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+
+BUILDROOT=$(pwd)
+
 mkdir -p build_temp
-cd build_temp
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../.. && cmake --build .
+cd build_temp || { echo "Failure:Wrong path"; exit 1; }
+
+NUMPROC=$(nproc)
+
+echo "Build root: $BUILDROOT"
+echo "Cores:      $NUMPROC "
+
+cmake $CMAKE_PARAMS "$BUILDROOT/.." && cmake --build . -- -j"$NUMPROC"
 

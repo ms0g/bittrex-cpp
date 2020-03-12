8068cb55cd36f80d8802cffe34b05bc61f38d9aa
msamigurpinar <sami.gurpinar@gmail.com>
2020-03-12 11:50:26 +0300

    added api type namespace

--- libbittrex/src/api/market.cpp
+++ libbittrex/src/api/market.cpp
@@ -3,7 +3,7 @@
 using namespace bittrex::api;
 
 uuid_t Market::buy_limit(const std::string &market, const float &quantity, const float &rate) {
-    auto res = api_request<uuid_t, uuid_t>(BUYLIMIT_END_P, Type::MARKET, "market=", market, "quantity=",
+    auto res = api_request<uuid_t, uuid_t>(BUYLIMIT_END_P, bittrex::api::Type::MARKET, "market=", market, "quantity=",
                                            quantity,
                                            "rate=", rate);
     return res;
@@ -12,7 +12,7 @@ uuid_t Market::buy_limit(const std::string &market, const float &quantity, const
 
 uuid_t Market::sell_limit(const std::string &market, const float &quantity, const float &rate) {
 
-    auto res = api_request<uuid_t, uuid_t>(SELLLIMIT_END_P, Type::MARKET, "market=", market, "quantity=",
+    auto res = api_request<uuid_t, uuid_t>(SELLLIMIT_END_P, bittrex::api::Type::MARKET, "market=", market, "quantity=",
                                            quantity,
                                            "rate=", rate);
     return res;
@@ -21,13 +21,13 @@ uuid_t Market::sell_limit(const std::string &market, const float &quantity, cons
 
 
 uuid_t Market::cancel(const std::string &uuid) {
-    auto res = api_request<uuid_t, uuid_t>(CANCEL_END_P, Type::MARKET, "uuid=", uuid);
+    auto res = api_request<uuid_t, uuid_t>(CANCEL_END_P, bittrex::api::Type::MARKET, "uuid=", uuid);
     return res;
 }
 
 
 open_order_list_t Market::get_open_orders(const std::string &market) {
-    auto res = api_request<open_order_list_t, open_order_t>(OPEN_ORDERS_END_P, Type::MARKET, "market=",
+    auto res = api_request<open_order_list_t, open_order_t>(OPEN_ORDERS_END_P, bittrex::api::Type::MARKET, "market=",
                                                             market);
     return res;
 }
--- libbittrex/src/api/public.cpp
+++ libbittrex/src/api/public.cpp
@@ -3,45 +3,45 @@
 using namespace bittrex::api;
 
 market_list_t Public::get_markets() {
-    auto res = api_request<market_list_t, market_t>(MARKET_END_P, Type::PUBLIC, "");
+    auto res = api_request<market_list_t, market_t>(MARKET_END_P, bittrex::api::Type::PUBLIC, "");
     return res;
 }
 
 
 currency_list_t Public::get_currencies() {
-    auto res = api_request<currency_list_t, currency_t>(CURRENCY_END_P, Type::PUBLIC, "");
+    auto res = api_request<currency_list_t, currency_t>(CURRENCY_END_P, bittrex::api::Type::PUBLIC, "");
     return res;
 }
 
 
 ticker_t Public::get_ticker(const std::string &market) {
-    auto res = api_request<ticker_t, ticker_t>(TICKER_END_P, Type::PUBLIC, "market=", market);
+    auto res = api_request<ticker_t, ticker_t>(TICKER_END_P, bittrex::api::Type::PUBLIC, "market=", market);
     return res;
 }
 
 
 market_sum_list_t Public::get_market_summaries() {
-    auto res = api_request<market_sum_list_t, market_sum_t>(MARKET_SUMS_END_P, Type::PUBLIC, "");
+    auto res = api_request<market_sum_list_t, market_sum_t>(MARKET_SUMS_END_P, bittrex::api::Type::PUBLIC, "");
     return res;
 
 }
 
 
 market_sum_list_t Public::get_market_summary(const std::string &market) {
-    auto res = api_request<market_sum_list_t, market_sum_t>(MARKET_SUM_END_P, Type::PUBLIC, "market=", market);
+    auto res = api_request<market_sum_list_t, market_sum_t>(MARKET_SUM_END_P, bittrex::api::Type::PUBLIC, "market=", market);
     return res;
 }
 
 
 order_book_t Public::get_order_book(const std::string &market, const std::string &type) {
-    auto res = api_request<order_book_t, order_book_t>(ORDER_BOOK_END_P, Type::PUBLIC, "market=", market,
+    auto res = api_request<order_book_t, order_book_t>(ORDER_BOOK_END_P, bittrex::api::Type::PUBLIC, "market=", market,
                                                        "type=", type);
     return res;
 }
 
 
 trade_list_t Public::get_market_history(const std::string &market) {
-    auto res = api_request<trade_list_t, trade_t>(MARKET_HIS_END_P, Type::PUBLIC,
+    auto res = api_request<trade_list_t, trade_t>(MARKET_HIS_END_P, bittrex::api::Type::PUBLIC,
                                                   "market=", market);
     return res;
 

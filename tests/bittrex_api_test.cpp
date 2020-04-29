d68010747964a13a1749d102ad5ee462d9c1f6a0
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2020-04-30 00:18:07 +0300

    fixed indentation

--- tests/bittrex_api_test.cpp
+++ tests/bittrex_api_test.cpp
@@ -7,54 +7,55 @@
 
 
 namespace bittrex {
-    namespace test {
-        bittrex::Client btxcli("", "");
-        auto p = btxcli.public_api();
-        const std::string &MARKET = "BTC-LTC";
+namespace test {
+
+bittrex::Client btxcli("", "");
+auto p = btxcli.public_api();
+const std::string &MARKET = "BTC-LTC";
 
 
-        BOOST_AUTO_TEST_SUITE(PublicApiTest)
+BOOST_AUTO_TEST_SUITE(PublicApiTest)
 
-            BOOST_AUTO_TEST_CASE(TestingGetMarkets) {
-                BOOST_REQUIRE_NO_THROW(p.get_markets());
-                auto markets = p.get_markets();
-                BOOST_CHECK(!markets.empty());
-            }
+BOOST_AUTO_TEST_CASE(TestingGetMarkets) {
+    BOOST_REQUIRE_NO_THROW(p.get_markets());
+    auto markets = p.get_markets();
+    BOOST_CHECK(!markets.empty());
+}
 
-            BOOST_AUTO_TEST_CASE(TestingGetCurrencies) {
-                BOOST_REQUIRE_NO_THROW(p.get_currencies());
-                auto currencies = p.get_currencies();
-                BOOST_CHECK(!currencies.empty());
+BOOST_AUTO_TEST_CASE(TestingGetCurrencies) {
+    BOOST_REQUIRE_NO_THROW(p.get_currencies());
+    auto currencies = p.get_currencies();
+    BOOST_CHECK(!currencies.empty());
+    
+}
 
-            }
+BOOST_AUTO_TEST_CASE(TestingGetTicker) {
+    BOOST_CHECK_NO_THROW(p.get_ticker(MARKET));
+}
 
-            BOOST_AUTO_TEST_CASE(TestingGetTicker) {
-                BOOST_CHECK_NO_THROW(p.get_ticker(MARKET));
-            }
+BOOST_AUTO_TEST_CASE(TestingGetMarketSummaries) {
+    BOOST_REQUIRE_NO_THROW(p.get_market_summaries());
+    auto market_summaries = p.get_market_summaries();
+    BOOST_CHECK(!market_summaries.empty());
+}
 
-            BOOST_AUTO_TEST_CASE(TestingGetMarketSummaries) {
-                BOOST_REQUIRE_NO_THROW(p.get_market_summaries());
-                auto market_summaries = p.get_market_summaries();
-                BOOST_CHECK(!market_summaries.empty());
-            }
+BOOST_AUTO_TEST_CASE(TestingGetMarketSummary) {
+    BOOST_CHECK_NO_THROW(p.get_market_summary(MARKET));
+}
 
-            BOOST_AUTO_TEST_CASE(TestingGetMarketSummary) {
-                BOOST_CHECK_NO_THROW(p.get_market_summary(MARKET));
-            }
+BOOST_AUTO_TEST_CASE(TestingGetOrderBook) {
+    BOOST_REQUIRE_NO_THROW(p.get_order_book(MARKET, "both"));
+    auto order_book = p.get_order_book(MARKET, "both");
+    BOOST_CHECK(!order_book.buy.empty());
+    BOOST_CHECK(!order_book.sell.empty());
+}
 
-            BOOST_AUTO_TEST_CASE(TestingGetOrderBook) {
-                BOOST_REQUIRE_NO_THROW(p.get_order_book(MARKET, "both"));
-                auto order_book = p.get_order_book(MARKET, "both");
-                BOOST_CHECK(!order_book.buy.empty());
-                BOOST_CHECK(!order_book.sell.empty());
-            }
+BOOST_AUTO_TEST_CASE(TestingGetMarketHistory) {
+    BOOST_REQUIRE_NO_THROW(p.get_market_history(MARKET));
+    auto market_history = p.get_market_history(MARKET);
+    BOOST_CHECK(!market_history.empty());
+}
 
-            BOOST_AUTO_TEST_CASE(TestingGetMarketHistory) {
-                BOOST_REQUIRE_NO_THROW(p.get_market_history(MARKET));
-                auto market_history = p.get_market_history(MARKET);
-                BOOST_CHECK(!market_history.empty());
-            }
-
-        BOOST_AUTO_TEST_SUITE_END()
-    }
+BOOST_AUTO_TEST_SUITE_END()
+}
 }

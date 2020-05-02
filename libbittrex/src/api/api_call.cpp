aceb4b8723a942b61e5eafb66328a988eb5272b1
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2020-05-02 04:55:33 +0300

    missing paranthesis

--- libbittrex/src/api/api_call.cpp
+++ libbittrex/src/api/api_call.cpp
@@ -6,30 +6,34 @@ std::string ApiCall::make_request(const std::string &key, const std::string &sec
                                   const std::string &endpoint, const bittrex::api::Type &type) {
     std::string res;
     auto wd = std::make_shared<curl::options::WriteData>(res);
-
+    
     std::string uri = BASE_URL + endpoint;
-
-    if (type != bittrex::api::Type::PUBLIC)
+    
+    if (type != bittrex::api::Type::PUBLIC) {
         uri += "apikey=" + key + "&nonce=" + std::to_string(std::time(nullptr)) +
-               "&" + payloads;
-    else uri += payloads;
-
+        "&" + payloads;
+    }
+    else {
+        uri += payloads;
+        
+    }
+    
     auto url = std::make_shared<curl::options::Url>(uri);
-
+    
     std::string apisign = "apisign:" + hmac_sha512(uri, secret);
     auto hh = std::make_shared<curl::options::HttpHeader>(apisign);
-
+    
     btx::Curl curl;
     curl.setOpt(hh);
     curl.setOpt(wd);
     curl.setOpt(url);
-
+    
     try {
         curl.perform();
     }
     catch (fail &e) {
         std::cout << e.what() << std::endl;
     }
-
+    
     return res;
 }
--- libbittrex/src/lib/libcurlpp.cpp
+++ libbittrex/src/lib/libcurlpp.cpp
@@ -12,20 +12,19 @@ size_t write_callback(char *contents, size_t size, size_t nmemb, void *userdata)
 
 
 Curl::Curl() :
-        m_curl(curl_easy_init(), [](CURL *ptr) {
-            curl_easy_cleanup(ptr);
-        }) {
-
-    if (!m_curl)
-        throw fail("Curl init failed!");
+m_curl(curl_easy_init(), [](CURL *ptr) {
+    curl_easy_cleanup(ptr);
+}) {
+    if (!m_curl) {
+        throw fail("Curl init failed!");}
 }
 
 void Curl::perform() {
     m_res = curl_easy_perform(m_curl.get());
     // Check for errors
-    if (m_res != CURLE_OK)
+    if (m_res != CURLE_OK) {
         std::cerr << "curl_easy_perform() failed: %s\n" << curl_easy_strerror(m_res) << std::endl;
-
+    }
 }
 
 

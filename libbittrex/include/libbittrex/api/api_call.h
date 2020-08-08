d91aba23357d988d59392bb8b1602c1de025c3f7
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2020-08-09 00:44:15 +0300

    refactored libcurl binding

--- libbittrex/include/libbittrex/api/api_call.h
+++ libbittrex/include/libbittrex/api/api_call.h
@@ -8,7 +8,7 @@
 #include <boost/property_tree/json_parser.hpp>
 #include <libbittrex/lib/exceptions.h>
 #include <libbittrex/api/api_type.h>
-#include <libbittrex/lib/libcurlpp.h>
+#include <libbittrex/lib/libcurlpp.hpp>
 #include <libbittrex/lib/hmac.h>
 
 
@@ -49,15 +49,17 @@ public:
     m_secret(std::move(secret)) {}
     
     template<typename ... Params>
-    void dispatch(const std::string &endpoint, const bittrex::api::Type &type, pt::ptree &json_tree, Params &&... rest) {
+    void dispatch(const char *endpoint, const bittrex::api::Type &type, pt::ptree &json_tree, Params &&... rest) {
         /* Create uri params */
         auto payloads = util::make_params(std::forward<Params>(rest)...);
         
         auto fut = std::async(std::launch::async, [&](){
             std::string res;
-            auto wd = std::make_shared<curl::options::WriteData>(res);
+            btx::Curl curl;
             
-            auto &&uri = BASE_URL + endpoint;
+            curl.setOpt<curl::options::WriteData>(res);
+            
+            auto &&uri = BASE_URL + std::string{endpoint};
             
             if (type != bittrex::api::Type::PUBLIC) {
                 uri += "apikey=" + m_key + "&nonce=" + std::to_string(std::time(nullptr)) + "&" + payloads.value_or("");
@@ -66,15 +68,11 @@ public:
                 
             }
             
-            auto url = std::make_shared<curl::options::Url>(uri);
+            curl.setOpt<curl::options::Url>(std::move(uri));
             
             auto &&apisign = "apisign:" + hmac_sha512(uri, m_secret);
-            auto hh = std::make_shared<curl::options::HttpHeader>(apisign);
+            curl.setOpt<curl::options::HttpHeader>(std::move(apisign));
             
-            btx::Curl curl;
-            curl.setOpt(hh);
-            curl.setOpt(wd);
-            curl.setOpt(url);
             
             try {
                 curl.perform();
---
+++ libbittrex/include/libbittrex/lib/libcurlpp.hpp
@@ -0,0 +1,130 @@
+#pragma once
+
+#include <libbittrex/lib/exceptions.h>
+
+#include <string>
+#include <curl/curl.h>
+#include <vector>
+#include <memory>
+#include <functional>
+
+static size_t write_callback(char *contents, size_t size, size_t nmemb, void *userdata) {
+    ((std::string *) userdata)->append(contents, size * nmemb);
+    return size * nmemb;
+}
+
+namespace curl::options {
+
+/**
+ * Base class for libcurl options
+ */
+class OptionBase {
+public:
+    virtual ~OptionBase() = default;
+
+    CURL *m_curlHandle;
+
+};
+
+/**
+ * Libcurl Http header Option
+ */
+class HttpHeader : public OptionBase {
+public:
+    using curl_slist_t = std::unique_ptr<curl_slist, std::function<void(curl_slist *)>>;
+
+    explicit HttpHeader(std::string &&header): m_header(std::move(header)) {
+        m_chunk = curl_slist_t{curl_slist_append(m_chunk.get(), m_header.c_str()),
+                               [](curl_slist *ptr) {
+                                   curl_slist_free_all(ptr);
+                               }};
+
+    }
+
+    inline void setOpt() {
+        curl_easy_setopt(m_curlHandle, CURLOPT_HTTPHEADER, m_chunk.get());
+    };
+
+private:
+    curl_slist_t m_chunk;
+    std::string m_header;
+};
+
+/**
+ * Libcurl WriteData Option
+ */
+class WriteData : public OptionBase {
+public:
+    explicit WriteData(std::string &buf) : m_buf(buf) {};
+
+    inline void setOpt() {
+        curl_easy_setopt(m_curlHandle, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(m_curlHandle, CURLOPT_WRITEDATA, &m_buf);
+    };
+
+private:
+    std::string &m_buf;
+};
+
+/**
+ * Libcurl Url Option
+ */
+class Url : public OptionBase {
+public:
+    explicit Url(std::string &&url) : m_url(std::move(url)) {};
+
+    inline void setOpt() {
+        curl_easy_setopt(m_curlHandle, CURLOPT_URL, m_url.c_str());
+    };
+
+private:
+    std::string m_url;
+};
+
+} //Namespace Curl
+
+namespace bittrex::lib {
+
+/**
+ * Wrapper class for libcurl
+ */
+class Curl {
+public:
+    Curl() {
+        if (!(m_curl = curl_easy_init())) {
+            throw fail("Curl init failed!");
+        }
+    }
+    ~Curl(){
+        curl_easy_cleanup(m_curl);
+        std::for_each(option_list.begin(), option_list.end(), [](curl::options::OptionBase *ptr){
+            delete ptr;
+        });
+        
+        option_list.clear();
+        
+    }
+
+    void perform(){
+        CURLcode res;
+        res = curl_easy_perform(m_curl);
+        // Check for errors
+        if (res != CURLE_OK) {
+               std::cerr << "curl_easy_perform() failed: %s\n" << curl_easy_strerror(res) << std::endl;
+        }
+    }
+
+    template<typename T, typename M>
+    void setOpt(M &&option) {
+        T *opt = new T{std::forward<M>(option)};
+        opt->m_curlHandle = m_curl;
+        opt->setOpt();
+        option_list.push_back(opt);
+    }
+
+private:
+    CURL *m_curl;
+    std::vector<curl::options::OptionBase*> option_list;
+};
+} //Namespace Bittrex
+
